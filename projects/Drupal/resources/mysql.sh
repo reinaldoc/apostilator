9306fbf0da41baf857e3e286e529fac635a5de01
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2017-10-07 18:48:54 -0300

    review drupal installation chapter

--- projects/Drupal/resources/mysql.sh
+++ projects/Drupal/resources/mysql.sh
@@ -1,5 +1,5 @@
 # mysql -p
-> CREATE DATABASE novoportal CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-> CREATE USER 'novoportal'@'localhost' IDENTIFIED BY 'top-secret';
-> GRANT SELECT, INSERT, UPDATE, DELETE, ALTER, CREATE, DROP, INDEX, LOCK TABLES, REFERENCES ON novoportal.* TO 'novoportal'@'localhost';
+> CREATE DATABASE portal CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+> CREATE USER 'portal'@'%' IDENTIFIED BY 'top-secret';
+> GRANT SELECT, INSERT, UPDATE, DELETE, ALTER, CREATE, DROP, INDEX, LOCK TABLES, REFERENCES ON portal.* TO 'portal'@'%';
 > QUIT;
